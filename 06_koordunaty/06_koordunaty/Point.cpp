6d82ec199db56fcb05d6e1ce3e0b713ae72f6856
Ivan Kosyk <ivan.kosyk2000@gmail.com>
2026-07-08 18:01:02 +0300

    New

--- 06_koordunaty/06_koordunaty/Point.cpp
+++ 06_koordunaty/06_koordunaty/Point.cpp
@@ -9,18 +9,31 @@ Point ReadPoint() {
     cin >> p.x >> p.y;
     return p;
 }
-void PrintPoint(Point p) {
-    cout << "{" << p.x << ", " << p.y << "}";
-}
+//void PrintPoint(Point p) {
+//    cout << "{" << p.x << ", " << p.y << "}";
+//}
 Vector2D PointDifference(Point p1, Point p2) {
     Vector2D v;
     v.x = p2.x - p1.x;
     v.y = p2.y - p1.y;
     return v;
 }
-void PrintVector(Vector2D v) {
-    cout << "(" << v.x << ", " << v.y << ")";
-}
+//void PrintVector(Vector2D v) {
+//    cout << "{" << v.x << ", " << v.y << "}";
+//}
 double FindLength(Vector2D v) {
     return hypot(v.x, v.y);
+}
+Triangle ReadTriangle() {
+    Triangle t;
+    t.p1 = ReadPoint();
+    t.p2 = ReadPoint();
+    t.p3 = ReadPoint();
+    return t;
+}
+double FindPerimeter(const Triangle& t) {
+    double a = FindLength(PointDifference(t.p1, t.p2));
+    double b = FindLength(PointDifference(t.p2, t.p3));
+    double c = FindLength(PointDifference(t.p3, t.p1));
+    return a + b + c;     
 }
--- 06_koordunaty/06_koordunaty/Point.h
+++ 06_koordunaty/06_koordunaty/Point.h
@@ -8,8 +8,15 @@ struct Vector2D {
     double x = 0;
     double y = 0;
 };
+struct Triangle {
+    Point p1;
+    Point p2;
+    Point p3;
+};
 Point ReadPoint();
-void PrintPoint(Point p);
 Vector2D PointDifference(Point p1, Point p2);
-void PrintVector(Vector2D v);
+//void PrintVector(Vector2D v);
 double FindLength(Vector2D v);
+
+Triangle ReadTriangle();
+double FindPerimeter(const Triangle& t);
